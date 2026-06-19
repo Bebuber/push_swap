5384a6fd7284b1aa8eb9a825db24c8307365d8fd
Betül Büber <164942537+Bebuber@users.noreply.github.com>
2026-06-19 02:04:55 +0200

    Cleanup: Remove commented-out main function
    Removed commented-out main function and related code.

--- ft_printf.c
+++ ft_printf.c
@@ -93,17 +93,3 @@ int	ft_printf(const char *c, ...)
 	va_end (args);
 	return (count);
 }
-
-//int	main(void)
-//{
-//	int	a;
-//	int b;
-
-//	a = ft_printf("%s", "");
-//	ft_printf("\n%d\n", a);
-
-//	b = printf("%s", "");
-//	printf("\n%d\n", b);
-//	return (0);
-//}
-// gcc -Wall -Wextra -Werror ft_printf.c libftprintf.a && ./a.out
